a88809f79c57442832083a4028875e250c8a65d6
Eric <ewade@stu.jsu.edu>
2025-09-08 20:24:49 -0500

    Added tooltip for MagicBlock

--- src/main/java/com/sylvypuppy/puppyscrazyaddons/block/custom/MagicBlock.java
+++ src/main/java/com/sylvypuppy/puppyscrazyaddons/block/custom/MagicBlock.java
@@ -3,6 +3,7 @@ package com.sylvypuppy.puppyscrazyaddons.block.custom;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
@@ -13,12 +14,16 @@ import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.item.context.UseOnContext;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
 
@@ -32,4 +37,9 @@ public class MagicBlock extends Block {
         return InteractionResult.FAIL;
     }
 
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.puppyscrazyaddons.magic_block.tooltip"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+    }
 }
